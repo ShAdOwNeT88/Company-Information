e9a87be29db815dc7dddc7e593b2d4d3be98f691
ShAdOwNeT88 <iovine.antonio.1988@gmail.com>
2016-03-20 15:08:46 +0100

    remove unused import

--- app/src/main/java/alterego/solutions/company_information/models/CompanyAdapter.java
+++ app/src/main/java/alterego/solutions/company_information/models/CompanyAdapter.java
@@ -2,17 +2,12 @@ package alterego.solutions.company_information.models;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 import alterego.solutions.company_information.Company;
